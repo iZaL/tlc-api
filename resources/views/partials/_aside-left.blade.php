3882579e1827bae6ed3a5575866861ad92347106
ZaL <z4ls@live.com>
2018-10-06 10:27:16 +0400

    add todo

--- resources/views/partials/_aside-left.blade.php
+++ resources/views/partials/_aside-left.blade.php
@@ -2,13 +2,15 @@
     <i class="la la-close"></i>
 </button>
 
-<div id="m_aside_left" class="m-grid__item	m-aside-left  m-aside-left--skin-dark ">
+<div id="m_aside_left" class="m-grid__item	m-aside-left  m-aside-left--skin-dark">
     <div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark " m-menu-vertical="1" m-menu-scrollable="0" m-menu-dropdown-timeout="500">
-        <ul class="m-menu__nav ">
+        <ul class="m-menu__nav">
+
             <li class="m-menu__section m-menu__section--first">
                 <h4 class="m-menu__section-text">Departments</h4>
                 <i class="m-menu__section-icon flaticon-more-v2"></i>
             </li>
+
             <li class="m-menu__item  m-menu__item--active" aria-haspopup="true">
                 <a href="{{ route('home') }}" class="m-menu__link ">
                     <span class="m-menu__item-here"></span>
@@ -16,11 +18,52 @@
                     <span class="m-menu__link-text">Dashboard</span>
                 </a>
             </li>
+
+            <li class="m-menu__item " aria-haspopup="true">
+                <a href="{{ route('home') }}" class="m-menu__link ">
+                    <span class="m-menu__item-here"></span>
+                    <i class="m-menu__link-icon flaticon-line-graph"></i>
+                    <span class="m-menu__link-text">Customers</span>
+                </a>
+            </li>
+
+            <li class="m-menu__item " aria-haspopup="true">
+                <a href="{{ route('home') }}" class="m-menu__link ">
+                    <span class="m-menu__item-here"></span>
+                    <i class="m-menu__link-icon flaticon-line-graph"></i>
+                    <span class="m-menu__link-text">Drivers</span>
+                </a>
+            </li>
+
+            <li class="m-menu__item " aria-haspopup="true">
+                <a href="{{ route('home') }}" class="m-menu__link ">
+                    <span class="m-menu__item-here"></span>
+                    <i class="m-menu__link-icon flaticon-line-graph"></i>
+                    <span class="m-menu__link-text">Loads</span>
+                </a>
+            </li>
+
+            <li class="m-menu__item " aria-haspopup="true">
+                <a href="{{ route('home') }}" class="m-menu__link ">
+                    <span class="m-menu__item-here"></span>
+                    <i class="m-menu__link-icon flaticon-line-graph"></i>
+                    <span class="m-menu__link-text">Routes</span>
+                </a>
+            </li>
+
+            <li class="m-menu__item " aria-haspopup="true">
+                <a href="{{ route('home') }}" class="m-menu__link ">
+                    <span class="m-menu__item-here"></span>
+                    <i class="m-menu__link-icon flaticon-line-graph"></i>
+                    <span class="m-menu__link-text">Trucks</span>
+                </a>
+            </li>
+
             <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover">
                 <a href="javascript:;" class="m-menu__link m-menu__toggle">
                     <span class="m-menu__item-here"></span>
                     <i class="m-menu__link-icon flaticon-layers"></i>
-                    <span class="m-menu__link-text">Resources</span>
+                    <span class="m-menu__link-text">Tr</span>
                     <i class="m-menu__ver-arrow la la-angle-right"></i>
                 </a>
                 <div class="m-menu__submenu ">
@@ -29,7 +72,7 @@
                         <li class="m-menu__item  m-menu__item--parent" aria-haspopup="true">
                             <span class="m-menu__link">
                                 <span class="m-menu__item-here"></span>
-                                <span class="m-menu__link-text">Resources</span>
+                                <span class="m-menu__link-text">Update Profile</span>
                             </span>
                         </li>
                         <li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1">
@@ -41,13 +84,7 @@
                     </ul>
                 </div>
             </li>
-            <li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1">
-                <a href="inner.html" class="m-menu__link ">
-                    <span class="m-menu__item-here"></span>
-                    <i class="m-menu__link-icon flaticon-suitcase"></i>
-                    <span class="m-menu__link-text">Finance</span>
-                </a>
-            </li>
+
         </ul>
     </div>
 </div>
---
+++ todos.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZaL
+ * Date: 10/6/18
+ * Time: 10:15 AM
+ */
+
+
+$todos = [
+    'CUSTOMERS' => [
+        'CRUD' => 'NO',
+    ],
+    'CUSTOMER_LOCATIONS' => [
+        'CRUD' => 'NO'
+    ],
+    'CUSTOMER_PAYMENTS' => [
+        'CRUD' => 'NO'
+    ],
+    'DRIVERS' => [
+        'CRUD' => 'NO'
+    ],
+    'DRIVER_DOCUMENTS' => [
+        'CRUD' => 'NO'
+    ],
+    'DRIVER_LANGUAGES' => [
+        'CRUD' => 'NO'
+    ],
+    'DRIVER_BLOCKED_DATES' => [
+        'CRUD' => 'NO'
+    ],
+    'DRIVER_ROUTES' => [
+        'CRUD' => 'NO'
+    ],
+    'DRIVER_ROUTE_LOCATIONS' => [
+        'CRUD' => 'NO'
+    ],
+    'DRIVER_SECURITY_PASSES' => [
+        'CRUD' => 'NO'
+    ],
+    'TRUCKS' => [
+        'CRUD' => 'NO'
+    ],
+    'TRUCK_MAKES' => [
+        'CRUD' => 'NO'
+    ],
+    'TRUCK_MODELS' => [
+        'CRUD' => 'NO'
+    ],
+    'TRAILERS' => [
+        'CRUD' => 'NO'
+    ],
+    'TRAILER_TYPES' => [
+        'CRUD' => 'NO'
+    ],
+    'TRAILER_MAKES' => [
+        'CRUD' => 'NO'
+    ],
+    'TRAILER_CARGOS' => [
+        'CRUD' => 'NO'
+    ],
+    'SECURITY_PASSES' => [
+        'CRUD' => 'NO'
+    ],
+    'COMMODITIES' => [
+        'CRUD' => 'NO'
+    ],
+    'PACKAGINGS' => [
+        'CRUD' => 'NO'
+    ],
+    'LOADS' => [
+        'CRUD' => 'NO',
+        'SECURITY_PASSES' => 'NO',
+        'SECURITY_REQUESTS' => 'NO',
+        'DOCUMENTS' => 'NO',
+    ],
+    'ROUTES' => [
+        'CRUD' => 'NO',
+        'TRANSITS' => 'NO'
+    ],
+    'DOCUMENT_TYPES' => [
+        'CRUD' => 'NO'
+    ],
+    'COUNTRIES' => [
+        'CRUD' => 'NO'
+    ],
+    'COUNTRY_DOCUMENTS' => [
+        'CRUD' => 'NO'
+    ],
+    'COUNTRY_FINES' => [
+        'CRUD' => 'NO'
+    ],
+    'COMMUNICATION_PROVIDERS' => [
+        'CRUD' => 'NO'
+    ],
+    'CARGO_TYPES' => [
+        'CRUD' => 'NO'
+    ],
+    'BANK_ACCOUNTS' => [
+        'CRUD' => 'NO'
+    ],
+    'EMPLOYEES' => [
+        'CRUD' => 'NO',
+        'COMMUNICATIONS' => 'NO',
+        'LANGUAGES' => 'NO',
+    ],
+    'LANGUAGES' => [
+        'CRUD' => 'NO'
+    ],
+    'TRANSACTIONS' => [
+        'CRUD' => 'NO'
+    ],
+    'TRIPS' => [
+        'CRUD' => 'NO',
+        'DOCUMENTS' => 'NO',
+        'FINES' => 'NO',
+    ],
+    'FINES' => [
+        'CRUD' => 'NO'
+    ],
+];
